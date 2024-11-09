ce6333414a5f59d87c37ff712cc215b74ae4a225
Naveen-005 <naveenalex512@gmail.com>
2024-11-09 07:48:52 +0530

    cors changes

--- app.js
+++ app.js
@@ -16,13 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+}));
 
 
 app.use('/', indexRouter);
