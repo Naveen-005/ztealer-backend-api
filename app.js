bdb50ce8e614aa2da07551eb21207e304877b45c
Naveen-005 <naveenalex512@gmail.com>
2024-11-09 08:18:02 +0530

    cors changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -32,3 +33,41 @@ app.use('/photos',photosRouter);
 app.use('/info',infoRouter);
 
 module.exports = app;
+*/
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const photosRouter = require('./routes/photos');
+const infoRouter = require('./routes/info');
+
+const app = express();
+
+// Apply CORS before other middleware and configure it for localhost
+app.use(cors({
+    origin: 'http://localhost:5173', // Update to your frontend's address
+    methods: 'GET,POST,PUT,DELETE',  // Specify allowed methods if needed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true
+}));
+
+// Other middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route handlers
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
+app.use('/info', infoRouter);
+
+module.exports = app;
+
